1e537722d8f573b2432febc9312607bd6103ad97
Seizerg <63910941+Seizerg@users.noreply.github.com>
2022-06-26 17:57:28 +0530

    netflix fv

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,12 +1,15 @@
 
 import React,{useState, useEffect} from 'react'
 import axios from "../axios"
-import "./Row.css"
+import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer"
 require("dotenv").config()
  
 
 function Row({title,fetched,isLargeRow}) {
- const [movies,setMovie]=useState([])
+ const [movies,setMovie]=useState([]);
+ const [trailerUrl, setTrailerUrl]= useState("");
 
 useEffect(()=>{
     async function fetchdata(){
@@ -17,7 +20,31 @@ useEffect(()=>{
     fetchdata();
 },[fetched])
 
+const opts={
+    height:"390",
+    width:"100%",
+    playerVars:{
+        autoplay:1
+    }
+}
 
+const handleClick= (movie)=>{
+    if(trailerUrl){
+        setTrailerUrl("");
+    }else{
+        movieTrailer(movie? (movie.name? movie.name : movie.title): "")
+        .then((url)=>{
+            console.log(url);
+            const urlParams= new URLSearchParams(new URL(url).search);
+            console.log(urlParams);
+            setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+        
+}
+}
 
     return (
         
@@ -27,12 +54,12 @@ useEffect(()=>{
          <div className="row-poster">
          {movies.map(movie=>{
             
-            return <img className={`row-posters ${isLargeRow&&"big-row"}`} key={movie.id} src={`${process.env.REACT_APP_BASE_URL}${isLargeRow?(movie?.poster_path):(movie?.backdrop_path)}`} alt={movie.name}/>
+            return <img className={`row-posters ${isLargeRow&&"big-row"}`} onClick={()=>handleClick(movie)} key={movie.id} src={`${process.env.REACT_APP_BASE_URL}${isLargeRow?(movie?.poster_path):(movie?.backdrop_path)}`} alt={movie.name}/>
             
          })}
         
          </div>
-            
+         {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}   
          
             
         </div>
